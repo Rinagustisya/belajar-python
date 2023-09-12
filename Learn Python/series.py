1ec0caeec34fb7eb1f9c1bbfb86eb459a4ccc447
Rina Agustisyaningsih <113598878+Rinagustisya@users.noreply.github.com>
2023-09-12 14:05:47 +0700

    add : series py

---
+++ Learn Python/series.py
@@ -0,0 +1,17 @@
+#program untuk menggabungkan beberapa list
+
+#import packages pandas
+
+import pandas as pd
+
+
+
+list1 = ["a", "b", "c", "d", "e", "f"]
+
+list2 = [1, 2, 3, 4, 5, 6]
+
+
+
+list_gabungan = list1 + list2
+
+list_gabungan
